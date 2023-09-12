c11157980d354f7d36b275c9117f6b1bf6d7f4f1
Vugar Gasimov <vugar.gasimov@avidatech.eu>
2023-09-12 15:30:07 +0300

    initial project setup

---
+++ beyonnex/anagram-x/src/main/java/io/beyonnex/anagramx/AnagramXApplication.java
@@ -0,0 +1,13 @@
+package io.beyonnex.anagramx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnagramXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnagramXApplication.class, args);
+	}
+
+}
